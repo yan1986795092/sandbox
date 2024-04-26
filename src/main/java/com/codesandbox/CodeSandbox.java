28ad22b2e7ab0ccb5489e09a528e923fbe89ab07
yan1986795092 <1986795092@qq.com>
2024-04-26 16:26:07 +0800

    Initial commit

---
+++ src/main/java/com/codesandbox/CodeSandbox.java
@@ -0,0 +1,19 @@
+package com.codesandbox;
+
+
+import com.codesandbox.model.ExecuteCodeRequest;
+import com.codesandbox.model.ExecuteCodeResponse;
+
+/**
+ * 代码沙箱接口定义
+ */
+public interface CodeSandbox {
+
+    /**
+     * 执行代码
+     *
+     * @param executeCodeRequest
+     * @return
+     */
+    ExecuteCodeResponse executeCode(ExecuteCodeRequest executeCodeRequest);
+}
